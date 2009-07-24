9199c8d821da237d758bc1b2d9a1e6553d7425ba
Steffen Vogel <info@steffenvogel.de>
2009-07-24 01:47:35 +0000

    fixed revision and date replacement by keywords and regex parsing

--- modules/footer/footer.php
+++ modules/footer/footer.php
@@ -1,7 +1,13 @@
 <?php
+
+preg_match("/(\d+)/", '$Revision$', $result);
+$rev = $result[1];
+
+preg_match("/\((.*)\)/", '$Date$', $result);
+$date = $result[1];
+
 echo '<div id="footer">
 		{lang:general:page_copy} - 
-		Rev.:$Revision$ ($Date$) - 
-		Code &amp; Design von <a href="http://www.steffenvogel.de/">Steffen Vogel</a>
+		CMS Revision: <a href="http://svn.griesm.de/tus">' . $rev . '</a> (' . $date . ') - Code &amp; Design von <a href="http://www.steffenvogel.de/">Steffen Vogel</a>
 	</div>';
 ?>
